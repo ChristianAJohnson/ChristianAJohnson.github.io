6ad893993f703de048e04de63040e63f4772810e
ChristianAJohnson <chrisgochris@gmail.com>
2019-01-03 15:49:50 -0500

    sucessfully tested onmouseover and onmouseout events to change the border style

--- js/projects.js
+++ js/projects.js
@@ -1,4 +1,11 @@
-function changeColor()
+function changeColor(i)
 {
     console.log("this is a test")
+    document.getElementsByClassName(i)[0].style.border = "8px solid #ffffff"
+}
+
+function changeBack(x) 
+{
+    console.log("this is the reset")
+    document.getElementsByClassName(x)[0].style.border = "2px solid #ffffff"
 }
