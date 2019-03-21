1b502bdc32f944bff485c3d1afda87aa458aa106
ChristianAJohnson <chrisgochris@gmail.com>
2019-03-21 11:56:46 -0400

    updated variables, id's and classes to be associated with projects

--- js/test.js
+++ js/test.js
@@ -1,28 +1,30 @@
 
 // Get the modal
-var modal = document.getElementById('myProjects');
+var projectmodal = document.getElementById('myProjects');
 
 // Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+var projectspan = document.getElementsByClassName("projectclose")[0];
 
 // When the user clicks on <span> (x), close the modal
-span.onclick = function () {
-    modal.style.display = "none";
+projectspan.onclick = function ()
+ {
+    projectmodal.style.display = "none";
 }
 
 // When the user clicks anywhere outside of the modal, close it
-window.onclick = function (event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
+window.onclick = function (event) 
+{
+    if (event.target == projectmodal) {
+        projectmodal.style.display = "none";
     }
 }
 
-function changeColor(i) {
-    console.log("this is a test")
+function changeColor(i) 
+{
     document.getElementsByClassName(i)[0].style.border = "8px solid #ffffff"
 }
 
-function changeBack(x) {
-    console.log("this is the reset")
+function changeBack(x) 
+{
     document.getElementsByClassName(x)[0].style.border = "2px solid #ffffff"
 }
