011044b6008aef7b449a9c73aaa08e8b9ef5c36d
ChristianAJohnson <chrisgochris@gmail.com>
2019-03-21 13:01:25 -0400

    upated About me link, to use Modal popup

--- js/test.js
+++ js/test.js
@@ -19,6 +19,24 @@ window.onclick = function (event)
     }
 }
 
+// Get the modal
+var infomodal = document.getElementById('myInfo');
+
+// Get the <span> element that closes the modal
+var infospan = document.getElementsByClassName("infoclose")[0];
+
+// When the user clicks on <span> (x), close the modal
+infospan.onclick = function () {
+    infomodal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+    if (event.target == infomodal) {
+        infomodal.style.display = "none";
+    }
+}
+
 function changeColor(i) 
 {
     document.getElementsByClassName(i)[0].style.border = "8px solid #ffffff"
