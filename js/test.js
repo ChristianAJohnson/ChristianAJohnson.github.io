06ace4685d23458f75500fbafc0da978935e2c4e
ChristianAJohnson <chrisgochris@gmail.com>
2019-03-21 11:27:16 -0400

    updated the test page so that modal is now working for the projects link

--- js/test.js
+++ js/test.js
@@ -1,3 +1,22 @@
+
+// Get the modal
+var modal = document.getElementById('myProjects');
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function () {
+    modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
 function changeColor(i) {
     console.log("this is a test")
     document.getElementsByClassName(i)[0].style.border = "8px solid #ffffff"
