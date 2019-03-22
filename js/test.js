d357777c517002ebca922b67bbdbdce905b70065
ChristianAJohnson <chrisgochris@gmail.com>
2019-03-22 08:41:38 -0400

    updated the skills link with the new modal skill pop-up

--- js/test.js
+++ js/test.js
@@ -1,41 +1,50 @@
-
-// Get the modal
 var projectmodal = document.getElementById('myProjects');
-
-// Get the <span> element that closes the modal
 var projectspan = document.getElementsByClassName("projectclose")[0];
 
-// When the user clicks on <span> (x), close the modal
 projectspan.onclick = function ()
- {
+{
     projectmodal.style.display = "none";
 }
 
-// When the user clicks anywhere outside of the modal, close it
 window.onclick = function (event) 
 {
-    if (event.target == projectmodal) {
+    if (event.target == projectmodal) 
+    {
         projectmodal.style.display = "none";
     }
 }
 
-// Get the modal
 var infomodal = document.getElementById('myInfo');
-
-// Get the <span> element that closes the modal
 var infospan = document.getElementsByClassName("infoclose")[0];
 
-// When the user clicks on <span> (x), close the modal
-infospan.onclick = function () {
+infospan.onclick = function () 
+{
     infomodal.style.display = "none";
 }
 
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function (event) {
-    if (event.target == infomodal) {
+window.onclick = function (event) 
+{
+    if (event.target == infomodal) 
+    {
         infomodal.style.display = "none";
     }
 }
+
+var skillsmodal = document.getElementById('mySkills');
+var skillspan = document.getElementsByClassName("skillsclose")[0];
+
+skillspan.onclick = function () {
+    skillsmodal.style.display = "none";
+}
+
+window.onclick = function (event) {
+    if (event.target == skillsmodal) {
+        skillsmodal.style.display = "none";
+    }
+}
+
+
+
 
 function changeColor(i) 
 {
