2216ceadc06926ff2ec034f2d08dc25b8b8110c9
ChristianAJohnson <chrisgochris@gmail.com>
2019-03-22 09:38:20 -0400

    removed the function for the modal to close, when clicking outside of the box. now only the x will close the box

--- js/test.js
+++ js/test.js
@@ -6,13 +6,14 @@ projectspan.onclick = function ()
     projectmodal.style.display = "none";
 }
 
-window.onclick = function (event) 
-{
-    if (event.target == projectmodal) 
-    {
-        projectmodal.style.display = "none";
-    }
-}
+// window.onclick = function (event) 
+// {
+//     if (event.target == projectmodal) 
+//     {
+//         projectmodal.style.display = "none";
+//         console.log("Project test modal");
+//     }
+// }
 
 var infomodal = document.getElementById('myInfo');
 var infospan = document.getElementsByClassName("infoclose")[0];
@@ -22,40 +23,47 @@ infospan.onclick = function ()
     infomodal.style.display = "none";
 }
 
-window.onclick = function (event) 
-{
-    if (event.target == infomodal) 
-    {
-        infomodal.style.display = "none";
-    }
-}
+// window.onclick = function (event) 
+// {
+//     if (event.target == infomodal) 
+//     {
+//         infomodal.style.display = "none";
+//     }
+// }
 
 var skillsmodal = document.getElementById('mySkills');
 var skillspan = document.getElementsByClassName("skillsclose")[0];
 
-skillspan.onclick = function () {
+skillspan.onclick = function () 
+{
     skillsmodal.style.display = "none";
 }
 
-window.onclick = function (event) {
-    if (event.target == skillsmodal) {
-        skillsmodal.style.display = "none";
-    }
-}
+// window.onclick = function (event) 
+// {
+//     if (event.target == skillsmodal) 
+//     {
+//         skillsmodal.style.display = "none";
+//     }
+// }
 
 var contactmodal = document.getElementById('myContact');
 var contactspan = document.getElementsByClassName("contactclose")[0];
 
-contactspan.onclick = function () {
+contactspan.onclick = function () 
+{
     contactmodal.style.display = "none";
+    console.log("Project test span");
 }
 
-window.onclick = function (event) {
-    if (event.target == contactmodal) {
-        contactmodal.style.display = "none";
-    }
-}
-
+// window.onclick = function (event) 
+// {
+//     if (event.target == contactmodal) 
+//     {
+//         contactmodal.style.display = "none";
+//         console.log("Project test modal");
+//     }
+// }
 
 
 function changeColor(i) 
